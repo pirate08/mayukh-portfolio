e9f1e08646127f184f18075a755a65039f36b5cd
Mayukh <mayukhdebgoswami82@gmail.com>
2026-02-23 15:21:16 +0530

    Font have been added...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold">Hello -- Mayukh Portfolio</h1>
-      <p className="text-xl mt-3">Coming soon...</p>
+      <h1 className="font-heading text-4xl font-bold">
+        Hello -- Mayukh Portfolio
+      </h1>
+      <p className="font-sans text-xl mt-3">
+        Coming soon...
+      </p>
     </div>
   );
 }
