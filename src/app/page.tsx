f48eed1a7b3af521b228a42f10ab7c7c4277fb9c
Mayukh <mayukhdebgoswami82@gmail.com>
2026-02-23 14:54:28 +0530

    Base code is ready for portfolio....

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">Hello -- Mayukh Portfolio</h1>
+      <p className="text-xl mt-3">Coming soon...</p>
+    </div>
+  );
+}
