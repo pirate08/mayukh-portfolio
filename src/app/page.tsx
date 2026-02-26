5fe88b2ba66f133409f6da061e6d444f5ebda661
Mayukh <mayukhdebgoswami82@gmail.com>
2026-02-27 00:15:28 +0530

    Base code is ready...W.I.P in all project page...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Footer from "@/components/layout/Footer";
+import Navbar from "@/components/layout/Navbar";
 import About from "@/components/sections/About";
 import Hero from "@/components/sections/Hero";
 import Projects from "@/components/sections/Projects";
@@ -6,12 +8,16 @@ import React from "react";
 export default function Home() {
   return (
     <div>
+      {/* --Navbar goes here-- */}
+      <Navbar />
       {/* --Hero Section goes here-- */}
       <Hero />
       {/* --About Section goes here-- */}
       <About />
       {/* --Projects Section goes here-- */}
       <Projects />
+      {/* --Footer goes here-- */}
+      <Footer />
     </div>
   );
 }
