a55b55cc66445aec90b4bd9ff2ddcfaa8f138506
Mayukh <mayukhdebgoswami82@gmail.com>
2026-03-19 18:56:58 +0530

    Added all projects fetch apis in strapi.ts file...

--- src/lib/strapi.ts
+++ src/lib/strapi.ts
@@ -1,3 +1,5 @@
+import { Project } from "@/types/project";
+
 const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_URL;
 
 // --Genric fetch function--
@@ -12,4 +14,20 @@ export async function fetchStrapi(endpoint: string) {
 
   const json = await res.json();
   return json.data;
+}
+
+// --Projects All fetch goes here--
+export async function getAllProjects(): Promise<Project[]> {
+  return fetchStrapi("projects?populate=image&sort=createdAt:desc");
+}
+
+export async function getProjectBySlug(slug: string): Promise<Project | null> {
+  const data = await fetchStrapi(
+    `projects?filters[slug][$eq]=${slug}&populate=image,screenshots`,
+  );
+  return data[0] ?? null;
+}
+
+export async function getFeaturedProjects(): Promise<Project[]> {
+  return fetchStrapi("projects?filters[isFeatured][$eq]=true&populate=image");
 }
