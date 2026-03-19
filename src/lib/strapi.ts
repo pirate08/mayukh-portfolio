1db5725d8ba0dcff618c4a7635fbc90cace85e66
Mayukh <mayukhdebgoswami82@gmail.com>
2026-03-19 18:25:49 +0530

    Genric fetch function from Strapi has been added...

--- src/lib/strapi.ts
+++ src/lib/strapi.ts
@@ -1,1 +1,15 @@
 const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_URL;
+
+// --Genric fetch function--
+export async function fetchStrapi(endpoint: string) {
+  const res = await fetch(`${STRAPI_URL}/api/${endpoint}`, {
+    next: { revalidate: 60 },
+  });
+
+  if (!res.ok) {
+    throw new Error(`Strapi fetched for: ${endpoint}`);
+  }
+
+  const json = await res.json();
+  return json.data;
+}
